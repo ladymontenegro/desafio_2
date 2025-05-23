b0c1be776d34d8a17581c817caf6a41a5ca2c4af
Vanessa Montenegro <vanessa.montenegro@udea.edu.co>
2025-05-22 23:50:40 -0500

    Implementacion de funcion cargarDatosAnfitriones

--- desarrollo_desafio_II/funciones_carga_archivos.cpp
+++ desarrollo_desafio_II/funciones_carga_archivos.cpp
@@ -3,6 +3,69 @@
 #include <string>
 using namespace std;
 
+//FUNCIONES PARA CARGA DE DATOS EN ESTRUCTURAS Y CLASES
+void cargarDatosAnfitriones(const string& rutaArchivo, Anfitrion* arregloAnfitriones[]) {
+    ifstream archivo(rutaArchivo);
+    if (!archivo.is_open()){
+        return -1;
+    }
+
+    short anfitrionesCargados = 0;
+    short alojamientosCargados = 0;
+    bool anfitrionActivo = false;
+    string linea;
+
+    while (getline(archivo, linea)) {
+        size_t inicio = 0;
+
+        if (esDocumento(linea, inicio)) { //pa ver si es anfitrion
+            if (!puedeAgregar(anfitrionesCargados, anfitriones)) {
+                cerr << "Limite de anfitriones alcanzado (" << anfitriones << ")\n";
+                continue;
+            }
+
+            //obtener atributos de anfitrion
+            string documento = obtenerDato(linea, inicio);
+            float puntuacion = stof(obtenerDato(linea, inicio));
+            short antiguedad = static_cast<short>(stoi(obtenerDato(linea, inicio)));
+
+            arregloAnfitriones[anfitrionesCargados] = new Anfitrion(documento, puntuacion, antiguedad);
+            anfitrionesCargados++;
+            alojamientosCargados = 0;  //se resetea pa contar cada uno de los alojamientos relacionados a un anfitrion
+            anfitrionActivo = true;
+        }
+        else if (anfitrionActivo) {  //si no es anfitrion, es alojamiento
+            if (!puedeAgregar(alojamientosCargados, alojamientosPorAnfitrion)) {
+                cerr << "Límite de alojamientos alcanzado para anfitrión " << arregloAnfitriones[anfitrionesCargados-1] -> getDocumento()<< " (" << alojamientosPorAnfitrion << ")\n";
+                continue;
+            }
+
+            //obtener atributos de alojamiento
+            string codigoAlojamiento = obtenerDato(linea, inicio);
+            string nombre = obtenerDato(linea, inicio);
+            string municipio = obtenerDato(linea, inicio);
+            string departamento = obtenerDato(linea, inicio);
+            string precioNoche = static_cast<unsigned int>(stoi(obtenerDato(linea, inicio)));
+            string tipo = obtenerDato(linea, inicio);
+            string direccion = obtenerDato(linea, inicio);
+            string amenidades = obtenerDato(linea, inicio);
+
+            //crear y agregar alojamiento
+            Alojamiento* nuevoAlojamiento = new Alojamiento(nombre, amenidades, codigoAlojamiento, municipio, departamento, tipo, direccion, documento, precioNoche);
+
+            if (arregloAnfitriones[anfitrionesCargados-1] -> agregarAlojamiento(nuevoAlojamiento)) {
+                alojamientosCargados++;
+            } else {
+                delete nuevoAlojamiento;  //libera memoria si no se pudo agregar
+            }
+        }
+    }
+
+    archivo.close();
+}
+
+//FUNCIONES PARA CARGA DE DATOS EN ARCHIVOS
+
 //FUNCIONES AUXILIARES
 string obtenerDato(const string& linea, size_t& inicio) {
     size_t fin = linea.find(',', inicio);
