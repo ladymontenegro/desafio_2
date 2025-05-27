8b96b91246b55859ea491e1f42f133407fc5b09b
Steven Hernandez Quintero <steven.hernandezq@udea.edu.co>
2025-05-27 18:01:18 -0500

    Complemento menu anfitriones

--- desarrollo_desafio_II/menuAnfitrion.cpp
+++ desarrollo_desafio_II/menuAnfitrion.cpp
@@ -20,14 +20,52 @@ void menuAnfitrion()
         } else {
             if (opcion == "3") {
                 cout << "Para salir por favor ingrese la fecha del dia de hoy: ";
-                cin >> fechaHoy;
-                // validar fecha
+                while (true) {
+                    cout << "Para salir por favor ingrese la fecha del dia de hoy: ";
+                    cin >> fechaHoy;
+                    try {
+                        Fecha fechaCorte = crearFecha(fechaHoy);
+                        break;
+                    } catch (const invalid_argument &excepcion) {
+                        cerr << "Fecha invalida. Intente de nuevo por favor" << excepcion.what()
+                             << " Intente de nuevo" << endl;
+                    } catch (const runtime_error &excepcion) {
+                        cerr << "Erro de ejcucion. Intente de nuevo por favor" << excepcion.what()
+                             << " Intente de nuevo" << endl;
+                    } catch (const exception &excepcion) {
+                        cerr << "A ocurrido un error desconocido: " << excepcion.what()
+                             << " Intente de nuevo" << endl;
+                    }
+                }
                 cout << "Saliendo del menu de anfitriones" << endl;
                 break;
             } else if (opcion == "1") {
                 while (true) {
+                    for (unsigned short i = 0;
+                         i < arregloAnfitriones[indiceAnfitrion]->getAlojamientosCargados;
+                         i++) {
+                        arregloAnfitriones[indiceAnfitrion]->getAlojamiento(i)->mostrarAlojamiento();
+                    }
+                    cout << "Ingrese el codigo del alojamiento que desea ver las reservas: ";
+                    string codigoAlojamiento;
+                    cin >> codigoAlojamiento;
                     cout << "Sus reservas son:" << endl;
-                    //mostar reservas
+                    for (unsigned short i = 0;
+                         i < arregloAnfitriones[indiceAnfitrion]->getAlojamientosCargados;
+                         i++) {
+                        if (arregloAnfitriones[indiceAnfitrion]->getAlojamiento(i)->getCodigo
+                            == codigoAlojamiento) {
+                            for (unsigned short j = 0; j < arregloAnfitriones[indiceAnfitrion]
+                                                               ->getAlojamiento(i)
+                                                               ->getReservasCargadas;
+                                 j++) {
+                                Reserva **reservasAlojamiento = arregloAnfitriones[indiceAnfitrion]
+                                                                    ->getAlojamiento(i)
+                                                                    ->getReservas();
+                                reservasAlojamiento[j]->mostarReserva;
+                            }
+                        }
+                    }
                     cout << "Escriba por favor el codigo de la reserva que desea anular: ";
                     string codigoReserva;
                     cin >> codigoReserva;
